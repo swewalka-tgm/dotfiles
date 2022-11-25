ed0afeee0a58e5451a8bfc7f619f8d7587558aea
swewalka-tgm <swewalka@student.tgm.ac.at>
2022-11-25 12:23:27 +0100

    walls und ja

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 cp -r ~/.config/i3 .
 cp -r ~/.config/ranger/ .
-cp -r ~/.config/wal/ .
 cp -r ~/.config/polybar .
-cp -r ~/.config/rofi .
 cp -r ~/.config/kitty/ .
 cp -r ~/.config/picom .
 cp -r ~/.config/nvim/lua/user/ ./nvim
 cp ~/.config/xborder/example_config.json ./xborder
 cp ~/.zshrc .
 cp -r ~/.fonts .
-
+cp -r ~/.oh-my-zsh .
+cp -r ~/Wallpapers/ .
