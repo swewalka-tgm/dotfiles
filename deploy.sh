be02643abaa3f0fd8b7b642aaf132e691c8c11d9
swewalka-tgm <swewalka@student.tgm.ac.at>
2022-11-25 11:27:13 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+cp -r ./i3 ~/.config/i3
+cp -r ./ranger/ ~/.config/ranger/ 
+cp -r ./wal ~/.config/wal/ 
+cp -r ./polybar/ ~/.config/polybar 
+cp -r ./rofi/ ~/.config/rofi 
+cp -r ./kitty/ ~/.config/kitty/ 
+cp -r ./picom/ ~/.config/picom 
+cp -r ./nvim/user/ ~/.config/nvim/lua/user/ 
+cp ./xborder/example_config.json ~/.config/xborder/example_config.json 
+cp .zshrc ~/.zshrc
+cp -r .fonts ~/.fonts/
+
